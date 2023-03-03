ac28dac0adca0d5dc794611029d1e4be40c59103
Mike Caballero <mikenoir34@gmail.com>
2023-03-02 21:29:29 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Footer template
+ *
+ * @package Aquila
+ */
+?>
+<footer>Footer</footer>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Main template file.
+ *
+ * @package Aquila
+ */
+
+get_header(); ?>
+<div class="content">
+Content
+</div>
+
+<?php
+
+get_footer();
+
